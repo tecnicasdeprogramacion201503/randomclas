7967daa62c6a131b7c58f9aec1332a994421651c
Santiago Meza <smezam@unal.edu.co>
2015-08-11 13:41:32 -0500

    se agrega archivo con los nombres del grupo y se modifica programa para seleccionarlos aletoriamente

--- aleatorio.py
+++ aleatorio.py
@@ -1,7 +1,7 @@
 import random
 def pick_winner():
 	lines = []
-	with open("list.txt", "r") as f:
+	with open("nombres.txt", "r") as f:
 		lines = f.readlines();
 	num = random.randrange(0, len(lines))
 	print lines[num]
