2577ba11ab47fe6f3b5582403b2d9a6b8290bd5b
cmmontoyag <cmmontoyag@unal.edu.co>
2015-08-13 22:39:19 -0500

    este es el archivo ejecutable corregido

---
+++ semana2/nombres_aleatorios/carlos_montoya.bat
@@ -0,0 +1,12 @@
+echo ahora vamos a seleccionar una persona
+python aleatorio.py > el_seleccionado.txt
+notepad el_seleccionado.txt
+
+echo ahora_vamos _a seleccionar un grupo de gente
+python aleatorio.py > los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+notepad los_seleccionados.txt
