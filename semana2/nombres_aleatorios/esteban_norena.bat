16070cfc7e63760118ef3409349b66f63ccec52e
esnoar <enorenaa@unal.edu.co>
2015-08-17 11:31:57 -0500

    corrección línea 3

--- semana2/nombres_aleatorios/esteban_norena.bat
+++ semana2/nombres_aleatorios/esteban_norena.bat
@@ -1,6 +1,6 @@
 echo ahora vamos a seleccionar una persona
 python aleatorio.py > el_seleccionado.txt
-erase "los_seleccioandos.txt"
+erase "los_seleccionados.txt"
 echo ahora_vamos _a seleccionar un grupo de gente
 python aleatorio.py > los_seleccionados.txt
 python aleatorio.py >> los_seleccionados.txt
