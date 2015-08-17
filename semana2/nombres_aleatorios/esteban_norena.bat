c0f1d48d563158379829f5a01a3ab49574d24f4c
esnoar <enorenaa@unal.edu.co>
2015-08-17 10:11:32 -0500

    Añado mi programa en .bat, punto 3 del correo enviado.

---
+++ semana2/nombres_aleatorios/esteban_norena.bat
@@ -0,0 +1,14 @@
+echo ahora vamos a seleccionar una persona
+python aleatorio.py > el_seleccionado.txt
+erase "los_seleccioandos.txt"
+echo ahora_vamos _a seleccionar un grupo de gente
+python aleatorio.py > los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+start notepad "los_seleccionados.txt"
+start notepad "el_seleccionado.txt"
+pause
+TASKKILL /F /IM notepad.exe
