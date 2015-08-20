73475d2fc06e71ea6974b2cc878a09e80c6fcc0a
nsilvap <nsilvap@unal.edu.co>
2015-08-20 07:41:00 -0500

    Se sube el archivo .bat y el programa para el calculo del sueldo real.

---
+++ semana2/bat/nicolas_silva_palacios.bat
@@ -0,0 +1,15 @@
+echo Ahora vamos a seleccionar una persona.
+python aleatorio.py > el_seleccionado.txt
+
+del "los_seleccionados.txt"
+echo Ahora vamos a seleccionar un grupo de gente.
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+cls
+
+start notepad el_seleccionado.txt
+start notepad los_seleccionados.txt
