fc81fd2752401f4c07b3925693093bf77c244ca5
jooha <agespinosar@unal.edu.co>
2015-08-20 01:42:49 -0500

    Se agrega programa python para salario, .bat y se corrige el nombre del posible compañero

---
+++ semana2/bat/Giovanna_Espinoa.bat
@@ -0,0 +1,17 @@
+echo Ahora vamos a seleccionar una persona.
+python aleatorio.py > el_seleccionado.txt
+
+del "los_seleccionados.txt"
+echo Ahora vamos a seleccionar un grupo de gente.
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+cls
+rem path = %path%;C:\Program Files (x86)\Notepad++
+rem notepad++ el_seleccionado.txt
+rem notepad++ los_seleccionados.txt
+start notepad el_seleccionado.txt
+start notepad los_seleccionados.txt
