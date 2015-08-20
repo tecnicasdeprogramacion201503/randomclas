d4edf40d4d858a9652b0fb7f2c08422998f66f04
sachimeza <smezam@unal.edu.co>
2015-08-20 01:33:15 -0500

    .bat para seleccionar personas al azar

---
+++ semana2/bat/santiago_meza.bat
@@ -0,0 +1,13 @@
+echo ahora vamos a seleccionar una persona
+python aleatorio.py > el_seleccionado.txt
+
+erase "los_seleccionados.txt"
+echo ahora_vamos _a seleccionar un grupo de gente
+python aleatorio.py > los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+notepad el_seleccionado.txt
+notepad los_seleccionados.txt
