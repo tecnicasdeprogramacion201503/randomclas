d30c3a287d08242149dddd6e5f742a2e96b5d0f5
mcamachoe <mcamachoe@unal.edu.co>
2015-08-19 23:26:49 -0500

    actividades semana 2 y semana 3

---
+++ semana2/nombres_aleatorios/Estefania_imbachi.bat
@@ -0,0 +1,11 @@
+echo Ahora vamos a seleccionar un grupo de gente.
+del los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+python aleatorio.py >> los_seleccionados.txt
+notepad los_seleccionados.txt
+cls
+
